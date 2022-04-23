a77b01a489b28c57003bef6d767b66193cec88f8
Jose <joavelar@ucsc.edu>
2022-04-22 22:53:53 -0700

    Switching lanes is pretty good now

--- src/prefab/Knight.js
+++ src/prefab/Knight.js
@@ -1,11 +1,25 @@
 class Knight extends Phaser.GameObjects.Container {
     constructor(scene) {
         super(scene);        
-        this.isFiring = false;      //swinging weapon
+        this.isSwinging = false;      //swinging weapon
         this.torso = new Phaser.GameObjects.Rectangle(scene, 0, -5, 10, 10, 0xFF0000);
         this.legs = new Phaser.GameObjects.Rectangle(scene, 0, 5, 10, 10, 0xFFFFFF);
         this.add(this.torso);
         this.add(this.legs);
+    }
+    
+    update() {
+        if(Phaser.Input.Keyboard.JustDown(keyW)) {//&& this.y == 438
+            this.y -= 123;
+            console.log(this.y);
+        }else if (Phaser.Input.Keyboard.JustDown(keyS)) {
+            this.y += 123;
+            console.log(this.y)
+        }
+        //if(Phaser.Input.Keyboard.JustDown(keyW) && this.y == 315 ){
+            //this.y -= 123;
+           // console.log(this.y);
+        //}
     }
 
 }
--- src/scenes/Play1.js
+++ src/scenes/Play1.js
@@ -11,7 +11,7 @@ class Play extends Phaser.Scene {
 
         this.add.text(20, 20, "cryptic crawler play scene1");
         this.add.rectangle(10, borderPadding, game.config.width - 20, borderUISize*1.8, 0x767676).setOrigin(0, 0);
-        //lower level
+        //lower level start level
         this.add.rectangle(0, 480-(borderPadding*3), game.config.width, borderPadding*2, 0x00FF00).setOrigin(0, 0);
         //second level
         this.add.rectangle(0, 320, game.config.width, borderPadding*2, 0x00FF00).setOrigin(0, 0);
@@ -20,12 +20,15 @@ class Play extends Phaser.Scene {
 
         //this.player1 = this.add.rectangle(10, 480-(borderPadding*3)- 10, 10, 10, 0xFFF).setOrigin(0.5);
         this.knight = new Knight(this, 100, 100);
-        this.knight.setPosition(15, 480-(borderPadding*3)-5);
+        this.knight.setPosition(15, 438);
+        console.log(480-(borderPadding*3)-5);
         this.add.existing(this.knight);
     
     }
     update() {
         //this.knight.angle += 1;
-        this.knight.torso.angle += 1;
+        //this.knight.torso.angle += 1; moves the upper torso
+        //this.knight.y -= 1;
+        this.knight.update()
     }
 }
