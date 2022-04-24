9a4e6ac91028102b26ab6b7d8b49cc1c52a8c566
Jose <joavelar@ucsc.edu>
2022-04-24 10:39:56 -0700

    beartrap changes lanes after it hits border

--- src/prefab/Beartrap.js
+++ src/prefab/Beartrap.js
@@ -5,11 +5,17 @@ class Beartrap extends Phaser.GameObjects.Sprite{
         this.moveSpeed = 1;
     }
 
+    getRandomInt(max) {//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+        return Math.floor(Math.random() * max);
+      }
+
     update() {
+        let Lanes = [438,192,315];
         //move left 
         this.x -= this.moveSpeed;
         //wrap around left
         if(this.x <= 0 - this.width){
+            this.y = Lanes[this.getRandomInt(3)]
             this.x = game.config.width;
         }
     }
