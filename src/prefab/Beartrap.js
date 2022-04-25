f78c96fffe86efb49be8cbab617de2023b381f9d
Jose <joavelar@ucsc.edu>
2022-04-24 22:12:24 -0700

    newest upload changed color for ground

--- src/prefab/Beartrap.js
+++ src/prefab/Beartrap.js
@@ -2,7 +2,7 @@ class Beartrap extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, texture, frame){
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
-        this.moveSpeed = 1;
+        this.moveSpeed = 1.49;
     }
 
     getRandomInt(max) {//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
--- src/prefab/Monster.js
+++ src/prefab/Monster.js
@@ -2,12 +2,26 @@ class Monster extends Phaser.GameObjects.Container{
     constructor(scene){
         super(scene);
         this.isSwinging = false;      //swinging weapon
-        this.torso = new Phaser.GameObjects.Rectangle(scene, 0, -5, 10, 10, 0xFF0000);
-        this.legs = new Phaser.GameObjects.Rectangle(scene, 0, 5, 10, 10, 0xFFFFFF);
+        this.torso = new Phaser.GameObjects.Rectangle(scene, 0, -5, 10, 10, 0x0000FF);
+        this.legs = new Phaser.GameObjects.Rectangle(scene, 0, 5, 10, 10, 0x00FF00);
         this.add(this.torso);
         this.add(this.legs);
+        this.moveSpeed = 1.5
     }
+
+    getRandomInt(max) {//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+        return Math.floor(Math.random() * max);
+      }
+
     update(){
+        let Lanes = [438,192,315];
+        //move left 
+        this.x -= this.moveSpeed;
+        //wrap around left
+        if(this.x <= 0 - this.width){
+            this.y = Lanes[this.getRandomInt(3)];
+            this.x = game.config.width;
+        }
         // if(MonsterID == 'flying'){
         //     this.x -= 5;
             
@@ -22,5 +36,11 @@ class Monster extends Phaser.GameObjects.Container{
         //         this.x = this.width
         //     }
         //}
+    }
+
+    reset(){
+        let Lanes = [438,192,315];
+        this.y = Lanes[this.getRandomInt(3)]-10
+        this.x = game.config.width;
     }
 }
