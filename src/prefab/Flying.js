98b2cdfc04cff49d318c66de82d3ab2a724cb8e1
Jose <joavelar@ucsc.edu>
2022-04-27 12:58:07 -0700

    the flying enemy now changes lanes

--- src/prefab/Flying.js
+++ src/prefab/Flying.js
@@ -3,13 +3,20 @@ class Flying extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture)
         scene.add.existing(this);
         this.moveSpeed = 1;
+        this.scale = 0.5;
     }
 
+    getRandomInt(max) {//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+        return Math.floor(Math.random() * max);
+      }
+
     update() {
+        let Lanes = [438,192,315];
         // move left
         this.x -= this.moveSpeed;
         // wrap around from left edge to right edge
         if(this.x <= 0 - this.width) {
+            this.y = Lanes[this.getRandomInt(3)];
             this.x = game.config.width;
         }
     }
