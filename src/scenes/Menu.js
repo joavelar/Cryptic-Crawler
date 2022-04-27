925a1bcf4e132a28188d913697c40a538965243e
JJHung999 <jjhung@ucsc.edu>
2022-04-27 12:40:40 -0700

    Added a menu, left line 18 function so you can skip straight to the play scene for testing purposes

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,8 +2,25 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene")
     }
+    preload(){
+        //load menu image
+        this.load.image('menu','./asset/UserInt/Menu.png')
+    }
     create() {
-        this.add.text(20, 20, "Crypt Crawler Menu Scene");
-        this.scene.start("playScene");
+
+        //create the menu image
+        this.Menu = this.add.sprite(game.config.width/2, game.config.height/2, 'menu')
+
+        //define the w key
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+
+        //placeholder skip past menu into playscene
+        //this.scene.start("playScene");
+    }
+    update(){
+        //press w to start game
+        if(Phaser.Input.Keyboard.JustDown(keyW)){
+            this.scene.start("playScene");
+        }
     }
 }
