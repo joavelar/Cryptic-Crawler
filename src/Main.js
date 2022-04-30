20d59a1aadb0fb18a99272250d620d374c30d68b
JJHung999 <jjhung@ucsc.edu>
2022-04-29 21:05:32 -0700

    Updated the header in main.js

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,8 @@
 //Title: Crypt Crawler
 //Authors: Tomas Hickman, Jose Velarde-Ruiz, Jonathan Hung
 //Date Completed: 5/2/2022
-//Creative Tilt: Dark Fantasy inspired endless runner where you fight monsters until you die.
+//Creative Tilt: Dark Fantasy inspired endless runner where players can aim and attack monsters
+//, or change lanes to evade the monsters until they die. 
 
 let config = {
     type: Phaser.CANVAS,
