caf4f62476d9134df71d9223784a89a41ce7067b
JJHung999 <jjhung@ucsc.edu>
2022-04-29 20:48:00 -0700

    Included a header in main.js

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,8 @@
+//Title: Crypt Crawler
+//Authors: Tomas Hickman, Jose Velarde-Ruiz, Jonathan Hung
+//Date Completed: 5/2/2022
+//Creative Tilt: Dark Fantasy inspired endless runner where you fight monsters until you die.
+
 let config = {
     type: Phaser.CANVAS,
     width: 720,
